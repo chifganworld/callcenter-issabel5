58b4406ec0e8a7bb3e61fd8c1d73168a320cba91
Chifundo Ganiza <chifganworld@users.noreply.github.com>
2025-01-02 21:26:11 +0300

    change command for Debian

--- build/5.0/install-issabel-callcenter.sh
+++ build/5.0/install-issabel-callcenter.sh
@@ -6,7 +6,9 @@
 
 GITHUB_ACCOUNT='ISSABELPBX'
 
-yum -y install git >/dev/null
+# Install required packages for Debian
+apt-get update >/dev/null
+apt-get -y install git php-cli >/dev/null
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -23,18 +25,18 @@ cd /usr/src
 rm -rf callcenter
 git clone https://github.com/${GITHUB_ACCOUNT}/callcenter-issabel5.git callcenter 2>&1 >/dev/null
 cd /usr/src/callcenter
-chown asterisk.asterisk modules/* -R
+chown www-data.www-data modules/* -R
 cp -pr modules/* /var/www/html/modules
 mkdir -p /opt/issabel/
 mv setup/dialer_process/dialer/ /opt/issabel/
 chmod +x /opt/issabel/dialer/dialerd
-mkdir -p /etc/rc.d/init.d/
-mv setup/dialer_process/issabeldialer /etc/rc.d/init.d/
-chmod +x /etc/rc.d/init.d/issabeldialer
+mkdir -p /etc/init.d/
+mv setup/dialer_process/issabeldialer /etc/init.d/
+chmod +x /etc/init.d/issabeldialer
 mkdir -p /etc/logrotate.d/
 mv setup/issabeldialer.logrotate /etc/logrotate.d/issabeldialer
 mv setup/usr/bin/issabel-callcenter-local-dnc /usr/bin
-chown asterisk.asterisk /opt/issabel -R
+chown www-data.www-data /opt/issabel -R
 rm -rf /usr/share/issabel/module_installer/callcenter/
 mkdir -p    /usr/share/issabel/module_installer/callcenter/
 mv setup/   /usr/share/issabel/module_installer/callcenter/
@@ -45,7 +47,7 @@ issabel-menumerge /usr/share/issabel/module_installer/callcenter/menu.xml
 
 mkdir -p /tmp/new_module/callcenter
 cp -r /usr/share/issabel/module_installer/callcenter/* /tmp/new_module/callcenter/
-chown -R asterisk.asterisk /tmp/new_module/callcenter
+chown -R www-data.www-data /tmp/new_module/callcenter
 
 php /tmp/new_module/callcenter/setup/installer.php
 rm -rf /tmp/new_module
@@ -53,7 +55,23 @@ rm -rf /tmp/new_module
 # Be sure to set shell for user asterisk
 chsh -s /bin/bash asterisk 2>&1 >/dev/null
 
-# Add dialer to startup scripts, and enable it by default
-chkconfig --add issabeldialer
-chkconfig --level 2345 issabeldialer on
-service issabeldialer start
+# Create systemd service file for issabeldialer
+cat > /etc/systemd/system/issabeldialer.service << 'EOL'
+[Unit]
+Description=Issabel Dialer Service
+After=network.target mysql.service asterisk.service
+
+[Service]
+Type=forking
+ExecStart=/etc/init.d/issabeldialer start
+ExecStop=/etc/init.d/issabeldialer stop
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable and start the service using systemd
+systemctl daemon-reload
+systemctl enable issabeldialer
+systemctl start issabeldialer
